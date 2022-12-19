a6b643edae812a3c16438e068989f14042119c4b
Ben Bass <benbass@codedstructure.net>
2022-12-19 07:33:28 +0000

    Advent of Code - day 17 - part 2

--- src/day17.rs
+++ src/day17.rs
@@ -27,15 +27,30 @@ fn display(chamber: &[u8]) {
     println!("-------\n");
 }
 
-pub fn step1() {
-    let wind = read_chars("inputs/day17.txt");
+fn simulate(max_rocks: u64, wind: &[char], display: bool) -> usize {
     // Chamber represented as a vector of u8s where the high bit
     // should always be zero. chamber[0] is row immediately above
     // the ground.
     let mut chamber: Vec<u8> = vec![];
     let mut wind_idx = 0;
     let mut rock_type = 0;
-    for _ in 0..2022 {
+    for rock_count in 0..max_rocks {
+        if display {
+            // We're looking for loops. Assume that when the base is nearly
+            // 'full', that's an interesting / likely point of repeat which
+            // is fairly rare.
+            if chamber.last().unwrap_or(&0u8).count_ones() > 5 {
+                println!(
+                    "{}: {} / {}: {} ## {}",
+                    rock_count,
+                    wind_idx,
+                    rock_type,
+                    chamber.last().unwrap_or(&0),
+                    chamber.len()
+                );
+            }
+        }
+
         let mut falling: Vec<u8> = vec![];
 
         match rock_type {
@@ -125,9 +140,48 @@ pub fn step1() {
 
         rock_type = (rock_type + 1) % 5;
     }
-
-    //println!("{:?}", wind);
-    println!("Height: {}", chamber.len());
+    chamber.len()
 }
 
-pub fn step2() {}
+pub fn step1() {
+    let wind = read_chars("inputs/day17.txt");
+    let height = simulate(2022, &wind, false);
+    println!("Height: {}", height);
+}
+
+pub fn step2() {
+    let wind = read_chars("inputs/day17.txt");
+
+    // Simulate enought to find loops...
+    //simulate(5000, &wind, true);
+
+    // Analyze loops - with human input!
+    // Output: (rock count / wind_idx / rock_type / row value ## height)
+    // 241: 1377 / 1: 63 ## 395
+    // 666: 3829 / 1: 63 ## 1063
+    // 836: 4780 / 1: 63 ## 1335
+    // 1276: 7380 / 1: 63 ## 2027
+    // 1986: 1377 / 1: 63 ## 3178
+    // 2411: 3829 / 1: 63 ## 3846
+    // 2581: 4780 / 1: 63 ## 4118
+    // 3021: 7380 / 1: 63 ## 4810
+    // 3731: 1377 / 1: 63 ## 5961
+    // 4156: 3829 / 1: 63 ## 6629
+    // 4326: 4780 / 1: 63 ## 6901
+    // 4766: 7380 / 1: 63 ## 7593
+    // So with wind_idx/rock_idx/row value all the same, we're going to loop
+    // and that happens every 1745 rocks. (e.g. 1986 - 241).
+    // In that time height increases 2783 (e.g. 3178 - 395).
+
+    let cycle_length = 1745u64;
+    let height_increase = 2783u64;
+
+    let excess = 1000000000000u64 % cycle_length;
+    let multiple_height = 1000000000000u64 / cycle_length;
+
+    let excess_height = simulate(excess, &wind, false);
+    println!(
+        "Height: {}",
+        multiple_height * height_increase + excess_height as u64
+    );
+}
